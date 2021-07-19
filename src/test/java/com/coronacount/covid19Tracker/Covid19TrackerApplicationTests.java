e436fba3ac71e1e79c2fcd593ef76fba5b8f13bd
prabodhalahiru <prabodhalahiru119@gmail.com>
2021-07-19 14:53:31 +0530

    init

---
+++ src/test/java/com/coronacount/covid19Tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronacount.covid19Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
